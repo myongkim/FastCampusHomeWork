cd26dc6403c1653b6855d70a526cb7c45fc1c1f3
Isaac Kim <myong328@gmail.com>
2018-10-07 22:11:08 +0900

    segue modified testing

--- SeguePractice/SeguePractice/ViewController.swift
+++ SeguePractice/SeguePractice/ViewController.swift
@@ -9,17 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var displayOne: UILabel!
+    var displayNumber = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //@IBAction func unwindToViewController(_ sender: UIStoryboardSegue) {
+    //    guard let sourceVC = sender.source as? SecondViewController else { return }
+    //    countLabel.text = "\(sourceVC.data + 10)"
+    //}
+    //}
+
+    
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        guard let secondVC = segue.destination as? SecondViewController else { return }
+        
+        secondVC.displayNumber2 = Int(secondVC.displayNumber2) + 1
+        
+      
+        
+        
     }
-
-
+    
+    // since we already used the segue with the direct pulling, we don't need performSegue at this time
+//    override func performSegue(withIdentifier identifier: String, sender: Any?) {
+//        super.performSegue(withIdentifier: identifier, sender: sender)
+//
+//
+//    }
+    
+    
+    @IBAction func unwindToViewController(_ sender: UIStoryboardSegue) {
+        guard let secondVC = sender.source as? SecondViewController else { return }
+        
+        displayOne.text = "\(secondVC.displayNumber2 + 10)"
+      
+        
+        
+    }
+    
 }
+
+
+
+
+//
+////  override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+////    super.shouldPerformSegue(withIdentifier: identifier, sender: sender)
+////    return Int(countLabel.text!)! < 50 ? true : false
+////  }
+//
+//// MARK: Unwind
+//
+
 
