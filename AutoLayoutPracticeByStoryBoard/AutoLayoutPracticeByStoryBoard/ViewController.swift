27f619f5c223ec4b4ccb9bad22d9ada29c0dacd3
Isaac Kim <myong328@gmail.com>
2018-10-15 17:44:12 +0900

    autoLayoutByStoryBoard

---
+++ AutoLayoutPracticeByStoryBoard/AutoLayoutPracticeByStoryBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutPracticeByStoryBoard
+//
+//  Created by Isaac Kim on 15/10/2018.
+//  Copyright © 2018 Isaac Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
