4491f4ee0de0c381f70db9871ffe501af92821a9
Isaac Kim <myong328@gmail.com>
2018-10-15 21:29:38 +0900

    delegate has been added dismiss the numeric button has been added when taped outside of the textfield.

--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var euroLabel: UILabel!
+    @IBOutlet weak var yenLabel: UILabel!
+    @IBOutlet weak var poundLabel: UILabel!
+    
+    let poundRate = 0.69
+    let yenRate = 113.94
+    let euroRate = 0.89
+    var dollarAmount = 0.0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        inputTextField.delegate = self
+        inputTextField.keyboardType = UIKeyboardType.numberPad
+        
+    }
+    
+    // called when 'return' key is pressed
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //called when user taps outside the text field
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        print("tochesBegan")
+        
     }
 
-
+    @IBAction func convertCurrency(_ sender: UIButton) {
+        if let amount = Double(inputTextField.text!) {
+            dollarAmount = amount
+        }
+        poundLabel.text = "$\(dollarAmount * poundRate)"
+        yenLabel.text = "$\(dollarAmount * yenRate)"
+        euroLabel.text = "$\(dollarAmount * euroRate)"
+        dollarAmount = 0.0
+        
+        
+        
+    }
+    
+    
+    
 }
 
