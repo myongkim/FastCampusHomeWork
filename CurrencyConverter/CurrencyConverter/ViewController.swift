83cc621208202680df2010ff2394eb8276a4e5b5
Isaac Kim <myong328@gmail.com>
2018-10-15 21:02:16 +0900

    1. constraint UI has been implemented 2. currency conversion xcode has been created

---
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Isaac Kim on 15/10/2018.
+//  Copyright © 2018 Isaac Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
