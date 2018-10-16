193d84579be7cc692fde6344c08fb7fa8872bce9
Isaac Kim <myong328@gmail.com>
2018-10-16 10:32:29 +0900

    practiceFortheAutoLayout

---
+++ practiceFortheAutoLayout/practiceFortheAutoLayout/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  practiceFortheAutoLayout
+//
+//  Created by Isaac Kim on 16/10/2018.
+//  Copyright © 2018 Isaac Kim. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        window?.backgroundColor = .white
+//        window?.rootViewController = ViewController()
+//        window?.makeKeyAndVisible()
+//        
+//        
+        
+        return true
+    }
+
+   
+
+
+}
+
