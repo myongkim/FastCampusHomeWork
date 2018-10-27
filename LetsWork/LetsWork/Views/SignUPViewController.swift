6f7db421d8c6489a240fea3d78face8b33864619
Isaac Kim <myong328@gmail.com>
2018-10-27 15:19:09 +0900

    signUp page has been added

--- LetsWork/LetsWork/Views/SignUPViewController.swift
+++ LetsWork/LetsWork/Views/SignUPViewController.swift
@@ -10,12 +10,42 @@ import UIKit
 
 class SignUPViewController: UIViewController {
 
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var initialPassword: UITextField!
+    @IBOutlet weak var retypePassword: UITextField!
+    
+    let message = "Please retype the password"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
   
     }
     
-
+    @IBAction func signUpTapped(_ sender: UIButton) {
+        let savedFirstName = firstName.text
+        let savedLastName = lastName.text
+        let savedInitialPW = initialPassword.text
+        let savedretypePW = retypePassword.text
+        
+        
+        if savedInitialPW != savedretypePW {
+            let alertController = UIAlertController(title: "Password Does not Match", message: message, preferredStyle: .alert)
+            let dismiss = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            
+            
+            alertController.addAction(dismiss)
+            present(alertController, animated: true)
+            
+            
+            
+            
+            
+        }
+        
+        
+    }
+    
  
 }
