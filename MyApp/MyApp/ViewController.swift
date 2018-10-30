7c480d49120d19ffc39bcc736d6b1d8dd31d7dad
Isaac Kim <myong328@gmail.com>
2018-10-30 12:35:11 +0900

    my app practice for the scheme

---
+++ MyApp/MyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by Isaac Kim on 30/10/2018.
+//  Copyright © 2018 Isaac Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
