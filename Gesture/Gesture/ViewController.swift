143e2c2958021cb7f00269bb8338b937e6868882
Isaac Kim <myong328@gmail.com>
2018-11-01 20:06:56 +0900

    gesture exercise

---
+++ Gesture/Gesture/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Gesture
+//
+//  Created by Isaac Kim on 01/11/2018.
+//  Copyright © 2018 Isaac Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var touchNumber = 0
+    
+    @IBOutlet weak var countGesture: UILabel!
+    
+    @IBOutlet weak var coordinateGesture: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+   
+    @IBAction func numberOfCount(_ sender: UITapGestureRecognizer) {
+        
+//        let count = sender.numberOfTouches
+        
+        if touchNumber == 0 {
+            touchNumber += 1
+        } else {
+            touchNumber += 1
+        }
+        print(touchNumber)
+        countGesture.text = "\(touchNumber)"
+        
+        let location = sender.location(in: self.view)
+         print("location  : ",location)
+        coordinateGesture.text = "\(location)"
+        
+        
+    }
+    
+    
+    @IBAction func Reset(_ sender: UIButton) {
+         print("sender: ",sender)
+        
+        touchNumber = 0
+        countGesture.text = "\(touchNumber)"
+    }
+    
+    
+    
+}
+
