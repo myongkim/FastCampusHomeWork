be59c98b72ea9230420f7868fc218c98641a0dec
Isaac Kim <myong328@gmail.com>
2018-11-02 15:07:09 +0900

    animationPractice for button to move

---
+++ AnimationPractice/AnimationPractice/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  AnimationPractice
+//
+//  Created by Isaac Kim on 02/11/2018.
+//  Copyright © 2018 Isaac Kim. All rights reserved.
+//
+
+import UIKit
+
+struct Time {
+    static let short = 0.3
+    static let middle = 0.65
+    static let long = 1.0
+}
+
+private struct UI {
+    static let menuCount = 5
+    static let menuSize: CGFloat = 50
+    static let distance: CGFloat = 130
+    static let minScale: CGFloat = 0.2
+    
+}
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
