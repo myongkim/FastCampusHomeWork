6db4f4f515f5ff4a0440472a2683c6fdf626bd87
Isaac Kim <myong328@gmail.com>
2018-11-25 15:44:16 +0900

    kakao_login practice

---
+++ KakaoTalk_LogIn/KakaoTalk_LogIn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KakaoTalk_LogIn
+//
+//  Created by Isaac Kim on 25/11/2018.
+//  Copyright © 2018 Isaac Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
--- kakaoLogin.md
+++ kakaoLogin.md
@@ -2,3 +2,13 @@ bundle for
 
 com.mkfoundation
 
+
+
+<#comment#>
+
+파일 복사 링크 - 만약 다른 컴퓨터에 자신의 snipet을 넣고 싶을 경우
+
+###경로 command + shift + G
+
+~/Library/Developer/Xcode/UserData/CodeSnippets
+
